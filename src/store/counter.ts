1b3e5dd477783e87e20f1130b54e3bf3cd336fe4
eugenepash <Eugene.Pashkevich@1pt.com>
2022-12-30 23:16:33 +0300

    feat: add, delete and change complete todo

--- src/store/counter.ts
+++ src/store/counter.ts
@@ -20,5 +20,4 @@ class Counter {
 
 }
 
-
 export default new Counter()
--- src/store/todo.ts
+++ src/store/todo.ts
@@ -1,13 +1,50 @@
 import {makeAutoObservable} from "mobx";
+import {v1} from "uuid";
 
 class Todo {
+
+    public todos: Array<TypeTodo> = [
+        {
+            id: '1w',
+            title: "What's learn",
+            isDone: false
+        },
+        {
+            id: '1ww2e',
+            title: "What's drink",
+            isDone: true
+        },
+
+    ]
 
     constructor() {
         makeAutoObservable(this)
     }
 
+    public addTodo () {
+        this.todos.push(
+            {
+            id: v1(),
+                title: 'new name',
+                isDone: false})
+    }
+
+    public deleteTodo (id: string) {
+        this.todos = this.todos.filter(t => t.id !== id)
+    }
+
+    public completeTodo (id: string) {
+        this.todos = this.todos.map(t => t.id === id ? {...t, isDone: !t.isDone} : t)
+    }
 }
 
-
-
 export default new Todo()
+
+
+
+
+type TypeTodo = {
+    id: string
+    title: string
+    isDone: boolean
+}
